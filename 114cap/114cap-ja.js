08222a15a6008dbaae4a908a9d05217ab1c62fb5
exam <124855122+digitalexam@users.noreply.github.com>
2025-01-12 21:37:27 +0800

    Update 114cap-ja.js

--- 114cap/114cap-ja.js
+++ 114cap/114cap-ja.js
@@ -4,12 +4,12 @@ const app = express();
 app.use(express.json());
 
 const accounts = {
-    admins01: { name: '莊雋逸', role: '監試委員', redirect: 'https://example.com/1' },
-    admins02: { name: 'Irene Chen', role: '監試委員', redirect: 'https://example.com/2' },
-    admins03: { name: '張竣淵', role: '監試委員', redirect: 'https://example.com/3' },
-    adminc01: { name: '莊雋逸', role: '批閱人員', redirect: 'https://example.com/4' },
-    adminc02: { name: '王紀凱', role: '批閱人員', redirect: 'https://example.com/5' },
-    adminc03: { name: '林秀玲', role: '批閱人員', redirect: 'https://example.com/6' }
+    admins01: { name: '莊雋逸', role: '監試委員', redirect: 'https://drive.google.com/drive/folders/1W7cMivvSz6UQwBa_OqDcGznD0sApFWul?usp=drive_link' },
+    admins02: { name: 'Irene Chen', role: '監試委員', redirect: 'https://drive.google.com/drive/folders/1W7cMivvSz6UQwBa_OqDcGznD0sApFWul?usp=drive_link' },
+    admins03: { name: '張竣淵', role: '監試委員', redirect: 'https://drive.google.com/drive/folders/1W7cMivvSz6UQwBa_OqDcGznD0sApFWul?usp=drive_link' },
+    adminc01: { name: '莊雋逸', role: '批閱人員', redirect: 'https://drive.google.com/drive/folders/1D-dQr4Xev-73xjdk9_Cnlna478fkspOK?usp=drive_link' },
+    adminc02: { name: '王紀凱', role: '批閱人員', redirect: 'https://drive.google.com/drive/folders/1yYxu5JmZYzMnpoaXheboL71Z869mfMjk?usp=sharing' },
+    adminc03: { name: '林秀玲', role: '批閱人員', redirect: 'https://drive.google.com/drive/folders/1wXIqo39geICuwgrQwzmr9SlGJtoQfaCt?usp=sharing' }
 };
 
 app.post('/api/login', (req, res) => {
